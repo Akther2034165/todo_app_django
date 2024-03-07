a1598100ebef4727385f01074306ea679d4ee2c5
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-03-07 16:45:24 +0600

    updated

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from todo_app.views import home,all_todos,edit_todos,delete_todos
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('',home,name='home'),
     path('all_todos/',all_todos, name='all_todos'),
@@ -7,3 +9,4 @@ urlpatterns = [
     path('delete_todos/<int:id>',delete_todos, name='delete_todos')
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- todos/settings.py
+++ todos/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'todos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER' : 'postgres',
+        'PASSWORD' : 'E1E4D3faF5DcFAcC1AGa3gBEccA*dbDd',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT' : '54248'
     }
 }
 
@@ -119,10 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
