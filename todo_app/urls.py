0c7715933fd662c131d137b0b2b34643525ec10d
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-03-07 16:56:31 +0600

    updates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path 
 from todo_app.views import home,all_todos,edit_todos,delete_todos
-from django.conf.urls.static import static
-from django.conf import settings
 urlpatterns = [
     path('',home,name='home'),
     path('all_todos/',all_todos, name='all_todos'),
     path('edit_todos/<int:id>',edit_todos, name='edit_todos'),
     path('delete_todos/<int:id>',delete_todos, name='delete_todos')
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
